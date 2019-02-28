4a0bdab6303e0d7799d29b7d58788ffbbc3b3ecf
Matvey Zaharchenko <matvey.ff@gmail.com>
2019-02-28 18:47:11 +0300

    Change build scripts
    Build everything with build.sh in the project root. The executables will
    be placed in build/bin. All the build files will be inside build/
    directory.

--- bot/build.sh
+++ bot/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-export GOPATH=$(pwd) # TODO get script location
+export GOPATH=$(pwd)
 go get github.com/sahilm/fuzzy
 go get github.com/go-telegram-bot-api/telegram-bot-api
 go get golang.org/x/net/proxy
-go build
+
+pkg_path="$(dirname "$0"})"
+# go build requires the path to be relative
+rel_path="$(realpath --relative-to="$(pwd)" "$pkg_path")"
+go build -i "$rel_path"
 
---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+root="$(pwd)"
+if [ "$1" = "--clean" ]; then
+    rm -rf build/
+fi
+mkdir -p build
+mkdir -p build/bin
+
+# Build cutter
+cp cutter/cutter.py build/bin
+mkdir -p build/cutter
+cd build/cutter
+cmake ../../cutter -DCMAKE_BUILD_TYPE=Release
+make -j $(nproc)
+strip -s cutter
+cd "$root"
+cp build/cutter/cutter build/bin/
+
+# Build bot
+mkdir -p build/bot
+cd build/bot
+"$root"/bot/build.sh
+strip -s bot
+cd "$root"
+cp build/bot/bot build/bin
+
+echo "Build finished successfully"
