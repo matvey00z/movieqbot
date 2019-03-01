cf471fa8cb5cc725f0b7b70cb5e1a92c3cc49f7c
Matvey Zaharchenko <matvey.ff@gmail.com>
2019-03-01 21:19:46 +0300

    Add basic authentication

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -18,6 +19,8 @@ import (
 const (
 	max_matches = 5
 )
+
+var authList = make(map[int64]struct{})
 
 type tRecord struct {
 	id   uint64
@@ -109,6 +112,12 @@ func serve(dbname string, token string, proxyAddr string) {
 	update_config.Timeout = 60
 	updates, err := bot.GetUpdatesChan(update_config)
 	for update := range updates {
+		if _, ok := authList[int64(update.Message.From.ID)]; !ok {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+				"You're not authenticated")
+			bot.Send(msg)
+			continue
+		}
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
@@ -141,20 +150,40 @@ func serve(dbname string, token string, proxyAddr string) {
 	}
 }
 
+func getAuthList(authListFileName string) {
+	file, err := os.Open(authListFileName)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		id, err := strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			log.Panic(err)
+		}
+		authList[id] = struct{}{}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Panic(err)
+	}
+}
+
 func main() {
 	var dbname string
 	var token string
 	var proxyAddr string
+	var authListFileName string
 	flag.StringVar(&dbname, "dbname", "", "Database filename")
 	flag.StringVar(&token, "token", "", "Bot token")
 	flag.StringVar(&proxyAddr, "proxy", "", "SOCKS5 proxy address")
+	flag.StringVar(&authListFileName, "auth", "", "Authentication file")
 	flag.Parse()
-	if dbname == "" || token == "" {
+	if dbname == "" || token == "" || authListFileName == "" {
 		flag.Usage()
 		return
 	}
+	getAuthList(authListFileName)
 	serve(dbname, token, proxyAddr)
-	/*for _, fname := range search(dbname, query) {
-		fmt.Println(fname)
-	}*/
 }
